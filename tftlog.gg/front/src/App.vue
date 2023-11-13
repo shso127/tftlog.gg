d6a318c4972ddbc2f8336af00563039ed11a56c1
seso <thtjdgus0828@naver.com>
2023-11-13 17:32:02 +0900

    front 추가

---
+++ tftlog.gg/front/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  aaa
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+body {
+  margin : 0;
+  background : rgb(50,50,50);
+  color : white;
+}
+</style>
