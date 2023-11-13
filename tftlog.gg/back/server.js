81012fd87e6411ca990d2d796a4b636ba9e998ca
seso <thtjdgus0828@naver.com>
2023-11-13 18:44:02 +0900

    vue nodejs 연결

--- tftlog.gg/back/server.js
+++ tftlog.gg/back/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.listen(8080, () => {
     console.log('http://localhost:8080')
 })
 
-app.get('/', (req, res) => {
-  res.send('안녕')
-}) 
+app.use( '/', express.static( path.join(__dirname, '../front/dist') ));  
+// 이 부분이 없으면 아래코드에서 index.html을 로드하지 못한다.
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../front/dist/index.html'));  
+})
